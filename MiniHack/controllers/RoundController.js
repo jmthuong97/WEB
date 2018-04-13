a30e4f710489dc97cc58cc0108521de743d235dd
jmthuong97 <jmthuong97@gmail.com>
2018-04-13 21:58:41 +0700

    MiniHack

---
+++ MiniHack/controllers/RoundController.js
@@ -0,0 +1,58 @@
+
+const roundSchema = require("../models/RoundSchema");
+const mongoose = require('mongoose');
+
+let createRoundFirst = (score, callback) => {
+    // create new round first
+    let newRound = {
+        idGame: 0,
+        noRound: 1,
+        score: score
+    }
+    roundSchema.create(newRound, function (err, round) {
+        if (err) { console.log(err) }
+        callback(round.id);
+    });
+};
+
+let createRound = (idGame, noRound, callback) => {
+    // create new round
+    let newRound = {
+        idGame: idGame,
+        noRound: noRound,
+        score: [0, 0, 0, 0]
+    }
+    console.log(newRound)
+    roundSchema.create(newRound, function (err, round) {
+        if (err) { console.log(err) }
+        callback(round.id);
+    });
+};
+
+// ================================== UPDATE ================================== 
+let updateRound = (id, idRound) => {
+    roundSchema.findByIdAndUpdate(idRound, { $set: { idGame: id } }, { upsert: true }, function (err, result) {
+        if (err) { console.log(err) };
+    });
+}
+
+let updateScore = (idRound, score, index) => {
+    let arrayScore;
+    roundSchema.findOne({ _id: idRound }, function (err, obj) {
+        if (err) { console.log(err) };
+        obj.score[index] = score;
+        console.log(obj)
+        roundSchema.findByIdAndUpdate(idRound, { $set: { score: obj.score } }, { upsert: true }, function (err, result) {
+            if (err) { console.log(err) };
+            console.log(result)
+        });
+    });
+}
+
+
+module.exports = {
+    createRoundFirst,
+    createRound,
+    updateRound,
+    updateScore
+}
