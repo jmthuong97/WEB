4341b73e53ac0521b173e3dfee694555effbd048
jmthuong97 <jmthuong97@gmail.com>
2018-04-13 21:56:41 +0700

    MiniHack

--- MiniHack/models/GameSchema.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-
-const gameSchema = new Schema({
-    users: [{type: Schema.Types.ObjectId, ref: 'user'}],
-    rounds:[{type: Schema.Types.ObjectId, ref: 'round'}]
-});
-
-module.exports = mongoose.model("game", gameSchema);
